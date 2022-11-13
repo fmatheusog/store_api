2fba9140a7a286dc7cf48c96e7fca0bc1a615bbf
Felipe Matheus <fmatheusog@gmail.com>
2022-11-13 16:01:12 -0300

    feat: create Server class

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Server from './server';
+
+const server = new Server();
+
+const port = '3000';
+
+server.start(port);
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+import express, { Express } from 'express';
+
+class Server {
+  private app: Express = express();
+
+  public start(port: string) {
+    this.app.listen(port, () => {
+      console.log(`Server running at port ${port}`);
+    });
+  }
+}
+
+export default Server;
