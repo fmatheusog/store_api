f7a97cc419deb7087b7dcdca57745e52fa462fb7
Felipe Matheus <fmatheusog@gmail.com>
2022-11-17 14:08:55 -0300

    feat: create GenericRepository interface

--- src/repositories/GenericRepository.interface.ts
+++ src/repositories/GenericRepository.interface.ts
@@ -1,7 +1,7 @@
-export default interface GenericRepository {
-  findAll<T>(): Promise<T[]>;
-  findById<T>(id: number): Promise<T>;
-  create<T>(entity: T): Promise<T>;
-  updateById<T>(entity: T): Promise<boolean>;
+export default interface GenericRepository<T> {
+  findAll(): Promise<T[]>;
+  findById(id: number): Promise<T>;
+  create(entity: T): Promise<T>;
+  updateById(entity: T): Promise<boolean>;
   deleteById(id: number): Promise<boolean>;
 }
