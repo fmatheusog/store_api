da9595aee97eb50ba1ce1215c0b2eecbd96e60be
Felipe Matheus <fmatheusog@gmail.com>
2022-11-17 14:42:16 -0300

    feat: create UserService class

---
+++ src/services/UserService.ts
@@ -0,0 +1,26 @@
+import User from "../entities/User";
+import UserRepository from "../repositories/UserRepository.interface";
+
+export default class UserService {
+  constructor(private userRepository: UserRepository) { }
+
+  public async findAll() {
+    return this.userRepository.findAll();
+  }
+
+  public async findById(id: number) {
+    return this.userRepository.findById(id);
+  }
+
+  public async create(data: User) {
+    return this.userRepository.create(data);
+  }
+
+  public async update(data: User) {
+    return this.userRepository.updateById(data);
+  }
+
+  public async delete(id: number) {
+    return this.userRepository.deleteById(id);
+  }
+}
